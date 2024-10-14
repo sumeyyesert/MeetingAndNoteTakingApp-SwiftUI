e219ea7163d7a343cfe39a8aa26bc79a9054e441
sumeyyesert <sumeyeesert@gmail.com>
2024-10-14 15:35:57 +0300

    Initial Commit

---
+++ minusdotminuss/minusdotminussApp.swift
@@ -0,0 +1,17 @@
+//
+//  minusdotminussApp.swift
+//  minusdotminuss
+//
+//  Created by Sümeyye Sert on 14.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct minusdotminussApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
